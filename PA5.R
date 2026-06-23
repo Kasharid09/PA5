2bd69bfcd78a1d9c16c75dba25c5336480d0339f
Kasharid09 <denskt1@students.fscj.edu>
2026-06-23 14:54:39 -0400

    Add files via upload

---
+++ PA5.R
@@ -0,0 +1,52 @@
+#cop2073c - PA5
+#Kashari Denson
+#06/22/2026
+# creating a dataframe and reading/ writing to a csv file
+
+
+
+# create a dataframe
+myDataFrame <- data.frame(Type = 
+                            c("Element 1", "Element 2"),
+                          matrix(data = 1:4, 
+                                 nrow = 2, 
+                                 ncol = 2, 
+                                 byrow = F,
+                          dimnames = list(
+                            c("A", "B"), 
+                            c("C","D")
+                            )
+                          ),
+                          matrix(data = 4:8,
+                                 nrow = 2,
+                                 ncol = 2,
+                                 byrow = F,
+                                 dimnames = list(
+                                   c("A","B"),
+                                   c("C", "D"))
+                                 ),
+                          matrix(data = 9:12,
+                                 nrow = 2,
+                                 ncol = 2,
+                                 byrow = F,
+                                 dimnames = list(
+                                   c("A","B"),
+                                   c("C","D"))
+                                 ))
+myDataFrame
+
+# write dataframe to a file 
+writeFile <- write.table(x = myDataFrame, file = "C:/Users/kasha/Documents/Intro to Statistical Programming/PA5.csv", 
+                         sep = " ", 
+                         quote = FALSE, row.names=FALSE )
+
+# removing myDataFrame
+rm(myDataFrame)
+
+# reading file PA5 to view dataframe
+
+readFile <- read.csv(file = "C:/Users/kasha/Documents/Intro to Statistical Programming/PA5.csv", 
+                        header = F, stringsAsFactors = F)
+
+# view dataframe
+View(readFile)
